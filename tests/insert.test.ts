27e1393fd6558455554e9e297a608e7d5c374926
harryplusplus <harryplusplus@gmail.com>
2025-10-05 17:20:08 +0900

    test: Add select, insert

---
+++ tests/insert.test.ts
@@ -0,0 +1,44 @@
+import { Knex } from "knex";
+import { knexPGlite } from "../src";
+
+let db: Knex;
+
+beforeEach(async () => {
+  db = knexPGlite();
+  await db.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.text("name");
+  });
+});
+
+afterEach(async () => {
+  await db?.destroy();
+});
+
+test("insert", async () => {
+  expect(await db("users").insert({ name: "a" })).toEqual({
+    affectedRows: 1,
+    fields: [],
+    rows: [],
+  });
+});
+
+test("insert multiple rows", async () => {
+  expect(await db("users").insert([{ name: "a" }, { name: "b" }])).toEqual({
+    affectedRows: 2,
+    fields: [],
+    rows: [],
+  });
+});
+
+test("insert returning all", async () => {
+  expect(await db("users").insert({ name: "a" }).returning("*")).toEqual([
+    { id: 1, name: "a" },
+  ]);
+});
+
+test("insert returning id", async () => {
+  expect(await db("users").insert({ name: "a" }).returning("id")).toEqual([
+    { id: 1 },
+  ]);
+});
---
+++ tests/select.test.ts
@@ -0,0 +1,50 @@
+import { Knex } from "knex";
+import { knexPGlite } from "../src";
+
+let db: Knex;
+
+beforeEach(async () => {
+  db = knexPGlite();
+  await db.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.text("name");
+  });
+});
+
+afterEach(async () => {
+  await db?.destroy();
+});
+
+test("select empty", async () => {
+  expect(await db("users").select()).toEqual([]);
+});
+
+test("select all 1 matched", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").select()).toEqual([{ id: 1, name: "a" }]);
+});
+
+test("select all 2 matched", async () => {
+  await db("users").insert({ name: "a" });
+  await db("users").insert({ name: "b" });
+  expect(await db("users").select()).toEqual([
+    { id: 1, name: "a" },
+    { id: 2, name: "b" },
+  ]);
+});
+
+test("select id", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").select("id")).toEqual([{ id: 1 }]);
+  expect(await db("users as u").select("u.id")).toEqual([{ id: 1 }]);
+});
+
+test("select id, name", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").select("id", "name")).toEqual([
+    { id: 1, name: "a" },
+  ]);
+  expect(await db("users as u").select("u.id", "u.name")).toEqual([
+    { id: 1, name: "a" },
+  ]);
+});
