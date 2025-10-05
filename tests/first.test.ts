875532573b3f31c152c67784305a1e7f16f8ff4d
harryplusplus <harryplusplus@gmail.com>
2025-10-05 17:47:26 +0900

    test: Add first test

---
+++ tests/first.test.ts
@@ -0,0 +1,40 @@
+import { Knex } from "knex";
+import { knexPGlite } from "../src";
+
+let db: Knex;
+
+beforeEach(async () => {
+  db = knexPGlite();
+  await db.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.text("name");
+  });
+});
+
+afterEach(async () => {
+  await db?.destroy();
+});
+
+test("first empty", async () => {
+  expect(await db("users").first()).toEqual(undefined);
+});
+
+test("first matched", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").first()).toEqual({ id: 1, name: "a" });
+});
+
+test("first id", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").first("id")).toEqual({ id: 1 });
+  expect(await db("users as u").first("u.id")).toEqual({ id: 1 });
+});
+
+test("first id, name", async () => {
+  await db("users").insert({ name: "a" });
+  expect(await db("users").first("id", "name")).toEqual({ id: 1, name: "a" });
+  expect(await db("users as u").first("u.id", "u.name")).toEqual({
+    id: 1,
+    name: "a",
+  });
+});
