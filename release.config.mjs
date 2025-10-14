5b6d89427ecee3d2d0dcaeb21ddfaa5e22e99dad
harryplusplus <harryplusplus@gmail.com>
2025-10-14 17:07:24 +0900

    build: Add @semantic-release/git

--- release.config.mjs
+++ release.config.mjs
@@ -7,11 +7,10 @@ export default {
     "@semantic-release/release-notes-generator",
     "@semantic-release/npm",
     "@semantic-release/github",
-    "@semantic-release/changelog",
     [
       "@semantic-release/git",
       {
-        assets: ["package.json", "CHANGELOG.md"],
+        assets: ["package.json"],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
